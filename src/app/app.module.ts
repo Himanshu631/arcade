2ac549b0efdf6a1206bc0044a4c815858fa0294a
Himanshu631 <himanshupauljsr@gmail.com>
2024-04-23 07:54:43 +0530

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,6 +13,13 @@ import { FormsModule } from '@angular/forms';
 import { HooksComponent } from './hooks/hooks.component';
 import { TesthookComponent } from './hooks/testhook/testhook.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: '', component: ContainerComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: ContainerComponent},
+]
 
 @NgModule({
   declarations: [
@@ -24,13 +31,15 @@ import { FooterComponent } from './footer/footer.component';
     FilterComponent,
     HooksComponent,
     TesthookComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
     provideClientHydration()
